e32303553af3deccbb9247e993d9a48deac941af
Isaac Koome <koomeisaac16@gmail.com>
2025-06-09 10:36:17 +0300

    fixed the view event dynamic route handling issue

--- app/events/weddings/[id]/page.tsx
+++ app/events/weddings/[id]/page.tsx
@@ -1,15 +1,5 @@
-"use client";
+import EventDetailPage from "@/components/EventDetailPage";
 
-import AddEventForm from "@/components/AddEventForm";
-import { eventTypes } from "@/data/eventTypes";
-
-export default function AddWeddingPage() {
-  const config = eventTypes["weddings"];
-  return (
-    <AddEventForm
-      eventType="weddings"
-      titleLabel={config.titleLabel}
-      coupleOrPersonLabel={config.coupleOrPersonLabel}
-    />
-  );
+export default function WeddingsEventPage({ params }: { params: { id: string } }) {
+  return <EventDetailPage id={params.id} collectionName="weddings" />;
 }
